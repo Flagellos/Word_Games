f0e9b5f1cf0d78cb93f947d88d0dab8c88be8daf
unknown <john.mckenney@menloschool.org>
2020-09-12 00:42:21 -0700

    second commit

--- word_games.py
+++ word_games.py
@@ -1,4 +1,29 @@
 # with open("names.txt", 'r') as f:
 #     for x in f:
 #         print(x)
+with open("scrabble_list.txt", 'r') as f:
+
+    scrabble_list = []
+    for x in f:
+        scrabble_list.append(x)
+    alphabet = [a,	b,	c,	d,	e,	f,	g,	h,	i,	j,	k,	l,	m,	n,	o,	p,	q,	r,	s,	t,	u,	v,	w,	x,	y,	z,	]
+    def play():
+        print("welcome to ghost!")
+
+
+
+    def getUserLetter():
+        letter = input("gimme a letter")
+        if(len(letter) == 1 and letter.isalpha()):
+            return letter
+        else:
+            return getUserLetter()
+    def getValidLetters(current_word):
+        
+
+
+
+
+
+
 
